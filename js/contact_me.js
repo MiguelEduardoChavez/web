27f4c41a6b15ecfadf760a86a44dfda4895b674c
MiguelEduardoChavez <lalo_super@live.com>
2018-06-23 10:40:34 -0500

    agregando habilidades

--- js/contact_me.js
+++ js/contact_me.js
@@ -1,3 +1,5 @@
+$('.counter-count').each(function(){$(this).prop('Counter',0).animate({Counter:$(this).text()},{duration:2000,easing:'swing',step:function(now){$(this).text(Math.ceil(now))}})})
+
 $(function() {
 
   $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
